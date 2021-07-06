ef58ba023ee21a24bd852681bf47544765e34ea4
ColdShills <86977804+ColdShills@users.noreply.github.com>
2021-07-06 01:30:44 -0500

    Update to address Zotac's recent domain change
    Zotac's recently changed their domain back from us.zotacstore.com to zotacstore.com/us & this has caused the script to stop working with some browsers. Notably Brave.
    Special thanks to @44cortseverns44 (twitch) for the help!

--- tampermonkey/zotac.form_key.user.js
+++ tampermonkey/zotac.form_key.user.js
@@ -4,7 +4,7 @@
 // @version      2021061101
 // @description  try to take over the world!
 // @author       You
-// @match        https://us.zotacstore.com/*
+// @match        https://www.zotacstore.com/*
 // @icon         https://www.google.com/s2/favicons?domain=zotacstore.com
 // ==/UserScript==
 
